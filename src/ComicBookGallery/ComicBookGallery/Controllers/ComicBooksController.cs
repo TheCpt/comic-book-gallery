dabddcc08dac1054f90d2d10cadd47521cf027b3
Tyler Crider <tcrider@ussunsolar.com>
2021-12-08 12:43:20 -0600

    added detail action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello World! -the comic books controller";
+        }
     }
 }
