352bb133e4d82dd963df4f1f01db2fc4a43c8c1d
Tyler Crider <tcrider@ussunsolar.com>
2021-12-16 12:48:52 -0600

    Added Comic Bokk Deatail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello World! -the comic books controller";
+            return View();
         }
     }
 }
